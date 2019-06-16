e4b081ef824e2e2abbd7197b09fee3bc1b7c60d9
Benoît Linsey <benoitlinsey20@gmail.com>
2019-06-16 18:20:59 +0200

    predicting only last output, with non-standardized power average

--- model.py
+++ model.py
@@ -6,11 +6,12 @@ from keras.models import Sequential, load_model
 from keras.layers import Dense, Activation
 from keras.layers import LSTM
 from keras.optimizers import Adam
+from keras.utils import plot_model
 
 
 class PandasBatchGenerator(object):
 
-    def __init__(self, data, num_steps, attr_column, target_column, batch_size, skip_step):
+    def __init__(self, data, num_steps, attr_column, target_column, batch_size, skip_step, returnSequence=False):
         self.data = data
         self.num_steps = num_steps
         self.batch_size = batch_size
@@ -19,12 +20,17 @@ class PandasBatchGenerator(object):
 
         self.current_idx = 0
         self.skip_step = skip_step
+        self.returnSeq = returnSequence
         self.idx_errors = []
 
 
     def generate(self):
         x = np.zeros((self.batch_size, self.num_steps, len(self.attr_col)))
-        y = np.zeros((self.batch_size, self.num_steps, 1))
+        if not self.returnSeq:
+            y = np.zeros((self.batch_size, 1))
+        else:
+            raise Exception("not implemented")
+            y = np.zeros((self.batch_size, self.num_steps, 1))
         while True:
             i = 0
             while i < self.batch_size:
@@ -33,7 +39,10 @@ class PandasBatchGenerator(object):
 
                 try:
                     x[i, :, :] = self.data.loc[self.current_idx:self.current_idx + self.num_steps - 1, self.attr_col].values
-                    y[i, :, :] = self.data.loc[self.current_idx + 1:self.current_idx + self.num_steps, self.target_col].values
+                    if not self.returnSeq:
+                        y[i, 0] = self.data.loc[self.current_idx + self.num_steps, self.target_col].values
+                    else:
+                        y[i, :, :] = self.data.loc[self.current_idx + 1:self.current_idx + self.num_steps, self.target_col].values
                 except Exception as e:
                     print(e)
                     self.idx_errors.append(self.current_idx)
@@ -56,7 +65,9 @@ def get_config(df):
     "batch_size": 100,
     "attr": list(df.drop("Time", 1)),
     "time_steps": 6,
-    "num_epochs": 1}
+    "num_epochs": 3,
+    "skip_steps": 1
+    }
 
 
 def normalize(df):
@@ -70,26 +81,37 @@ def normalize(df):
 
 #def test_model(model, test_set):
 
-def get_trained_model(training_set, validation_set, config, loadFile=None):
+def get_trained_model(training_set, validation_set, config, loadFile=None, plotLoss=False):
     if loadFile:
         return load_model(loadFile)
 
     training_generator = PandasBatchGenerator(training_set, config["time_steps"], config["attr"],
-                                              ["Power average [kW]"], config["batch_size"], config["time_steps"])
+                                              ["Power_average"], config["batch_size"], config["skip_steps"])
     validation_generator = PandasBatchGenerator(validation_set, config["time_steps"], config["attr"],
-                                                ["Power average [kW]"], config["batch_size"], config["time_steps"])
+                                                ["Power_average"], config["batch_size"], config["skip_steps"])
 
     model = Sequential()
-    model.add(LSTM(100, return_sequences=True, input_shape=(config["time_steps"], len(config["attr"]))))
+    model.add(LSTM(500, input_shape=(config["time_steps"], len(config["attr"])), unroll=True, return_sequences=True))
+    model.add(LSTM(500, unroll=True))
+    #model.add(LSTM(500, stateful=True, batch_input_shape=(config["batch_size"], config["time_steps"], len(config["attr"]))))
+    #model.add(LSTM(500, stateful=True))
     model.add(Dense(1))
 #    model.add(Activation("tanh"))
 
     model.compile(loss='mean_squared_error', optimizer='adam')
     print(model.summary())
-    model.fit_generator(training_generator.generate(), len(training_set) // config["batch_size"], config["num_epochs"],
-                        validation_data=validation_generator.generate(),
-                        validation_steps=len(validation_set) // config["batch_size"])
+    history = model.fit_generator(training_generator.generate(), len(training_set) // (config["batch_size"]*config["skip_steps"]),
+                                  config["num_epochs"], validation_data=validation_generator.generate(),
+                                  validation_steps=len(validation_set) // (config["batch_size"]*config["skip_steps"]), shuffle=False)
     print("nb inputs skipped = %d" % (len(training_generator.idx_errors)))
+    if plotLoss:
+        plt.plot(history.history['loss'])
+        plt.plot(history.history['val_loss'])
+        plt.title('Model loss')
+        plt.ylabel('Loss')
+        plt.xlabel('Epoch')
+        plt.legend(['Train', 'Test'], loc='upper left')
+        plt.show()
 
     return model
 
@@ -105,25 +127,29 @@ def main():
         return 1
 
     df_mod = df.drop("Time", 1).apply(pd.to_numeric, 1, errors="coerce")
-    df_mod = normalize(df_mod)
-    df_mod = df_mod.assign(Time=df["Time"])
+    df_mod = normalize(df_mod.drop("Power average [kW]", 1))
+    df_mod = df_mod.assign(Time=df["Time"], Power_average=df["Power average [kW]"])
     df_mod.dropna(inplace=True)
 
     training_set, validation_set, test_set = separate_set_seq(df_mod)
 
     config = get_config(df_mod)
 
-    model = get_trained_model(training_set, validation_set, config)
+    model = get_trained_model(training_set, validation_set, config, plotLoss=True)
 
     #test_model(model, test_set)
 
     test_generator = PandasBatchGenerator(test_set, config["time_steps"], config["attr"],
-                                          ["Power average [kW]"], 1, config["time_steps"])
-    inp, out = next(test_generator.generate())
-    y = model.predict(inp, verbose=1)
-    print(y)
-    print(out)
-    model.save("full_model.h5")
+                                          ["Power_average"], 1, config["skip_steps"])
+    for i in range(1):
+        pd.set_option('display.max_columns', 500)
+        pd.set_option('display.width', 1000)
+        print(test_set[test_generator.current_idx:test_generator.current_idx + test_generator.num_steps + 1])
+        inp, out = next(test_generator.generate())
+        y = model.predict(inp, verbose=1)
+        print(y)
+        print(out)
+    #model.save("full_model.h5")
 
 if __name__ == "__main__":
     main()
