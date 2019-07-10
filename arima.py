da858bb8d084e28578232d3a25bb9ddf5c0f6bb4
Benoît Linsey <benoitlinsey20@gmail.com>
2019-07-10 16:08:59 +0200

    adding auto regressive model

--- arima.py
+++ arima.py
@@ -1,9 +1,8 @@
 import sys
 import matplotlib.pyplot as plt
 from preprocessing import *
-from statsmodels.tsa.arima_model import ARIMA
-from sklearn.metrics import mean_squared_error
-
+from statsmodels.tsa.statespace.sarimax import SARIMAX
+from sklearn.metrics import mean_squared_error, mean_absolute_error
 
 df = read_file(sys.argv[1])
 df = clean_data(df)
@@ -13,17 +12,30 @@ df = clean_data(df)
 df = df["target_variable"]
 
 training_set, validation_set, test_set = separate_set_seq(df)
+
 history = [x for x in training_set["target_variable"]]
-predictions = list()
-for t in range(len(test_set)):
-	model = ARIMA(history, order=(18,0,0))
-	model_fit = model.fit(disp=0)
-	output = model_fit.forecast()
-	yhat = output[0]
-	predictions.append(yhat)
-	obs = test_set[t]
-	history.append(obs)
-	print('predicted=%f, expected=%f' % (yhat, obs))
-error = mean_squared_error(test_set, predictions)
+test_set = test_set["target_variable"]
+validation_set = validation_set["target_variable"]
+
+model = SARIMAX(history, order=(18,0,0))
+model_fit = model.fit(disp=0)
+
+real_model = SARIMAX(validation_set, order=(18,0,0))
+res = real_model.filter(model_fit.params)
+
+predictions = []
+observations = []
+for n in range((len(validation_set) - 36) // 6):
+	output = res.get_prediction(start=n*6, dynamic=0, end=n*6+35).predicted_mean
+	for t in range(36):
+		yhat = output[n*6 + t]
+		predictions.append(yhat)
+		obs = validation_set[n*6 + t]
+		observations.append(obs)
+		#print('predicted=%f, expected=%f' % (yhat, obs))
+
+error = mean_squared_error(observations, predictions)
 print('Test MSE: %.3f' % error)
+mae = mean_absolute_error(observations, predictions)
+print('Test MAE: %.3f' % mae)
 # plot
