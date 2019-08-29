14f2ce6d3608a4ad86676282b9fb19045a6b2961
Benoît Linsey <benoitlinsey20@gmail.com>
2019-08-29 11:52:00 +0200

    removed unnecessary read of samples

--- visualization.py
+++ visualization.py
@@ -17,20 +17,18 @@ def plot_predicted_vs_truth(predicted, truth, miny, maxy, name=None):
     plt.close()
 
 def plot_relation_power_output(df, attr):
-    df.boxplot(by=attr, column="Power average [kW]")
+    df.boxplot(by=attr, column="Power average [kW]", showfliers=False)
+    axes = plt.gca()
     plt.show()
-
-df = read_file(sys.argv[1])
-df = arrange_data(df)
+    plt.close()
 
 #df["Wind average [m/s]"][0:100].plot()
 #plt.show()
 if __name__ == "__main__":
-    plt.plot(df.loc[0:143, "Wind average [m/s]"], label="Wind speed [m/s]")
-    plt.xticks([x * 6 for x in range(24)], labels=[x for x in range(24)])
-    plt.xlabel("hours")
-    plt.legend()
-    plt.show()
-#plot_relation_power_output(df, "hour")
+    df = read_file(sys.argv[1])
+    df = arrange_data(df)
+
+    plot_relation_power_output(df, "hour")
+
 #print(df["Power average [kW]"].mean())
 #print(df["Wind average [m/s]"].mean())
